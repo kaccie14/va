8dd57973aa43d8ac2055ab688c8b1d22b870aa28
Kaccie Y Li <kli@rxsight.com>
2020-01-12 18:39:54 -0800

    Initial commit

---
+++ VA Scale/VA Scale/ScaleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ScaleViewController.swift
+//  VA Scale
+//
+//  Created by Kaccie Y Li on 1/12/20.
+//  Copyright © 2020 Kaccie Y Li. All rights reserved.
+//
+
+import UIKit
+
+class ScaleViewController: UITableViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
